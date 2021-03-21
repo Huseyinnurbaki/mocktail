5d7b93baa5991fce0a530b4309bb3764f7fea3b4
Huseyin Haluk Nurbaki <huseyin.nurbaki@eteration.com>
2021-03-21 23:42:21 +0300

    maintenace

--- dockerfile
+++ dockerfile
@@ -1,4 +1,4 @@
-FROM node:14 AS builder
+FROM node:lts-slim AS builder
 
 WORKDIR /app
 ADD . ./
--- run.sh
+++ run.sh
@@ -9,14 +9,14 @@ cp -R ./client/build ./
 # # Build
 # docker build -t hhaluk/mocktail .
 
-docker build -t hhaluk/mocktail:latest -t hhaluk/mocktail:v1.0.2 .
+docker build -t hhaluk/mocktail:latest -t hhaluk/mocktail:v1.0.3 .
 
 # # Run
-  docker run -p 7080:7080 -d hhaluk/mocktail
+  docker run -p 7080:7080 hhaluk/mocktail
 
 # # Push
 #  docker push hhaluk/mocktail:latest
-#  docker push hhaluk/mocktail:v1.0.2
+#  docker push hhaluk/mocktail:v1.0.3
 
 
 
