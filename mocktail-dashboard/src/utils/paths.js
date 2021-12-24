6b6d9d69762360a3f06fcb67ba0b4ec38fd261ac
hhaluk <huseyinnurbaki@gmail.com>
2021-12-24 23:06:36 +0300

    Update: paths

--- mocktail-dashboard/src/utils/paths.js
+++ mocktail-dashboard/src/utils/paths.js
@@ -1,7 +1,11 @@
-export const API_MOCKTAIL_URL = 'http://localhost:3200/mocktail';
-const API_CORE_URL = 'http://localhost:3200/core/v1';
+/* eslint-disable no-undef */
+const BASE_URL = process.env.NODE_ENV === 'development' ? 'http://localhost:4002' : '';
+
+const API_CORE_URL = `${BASE_URL}/core/v1`;
 
 export const ALL_APIS = `${API_CORE_URL}/apis`;
 export const SAVE_API = `${API_CORE_URL}/api`;
 export const DELETE_API = `${API_CORE_URL}/api`;
 export const EXPORT_API = `${API_CORE_URL}/export`;
+
+export const API_MOCKTAIL_URL = `${BASE_URL}/mocktail`;
